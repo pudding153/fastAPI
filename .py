26388cb6ca800652f9e851ef4c57a796710c4d93
pudding153 <subiriam777@gmail.com>
2026-06-27 20:22:08 +0900

    Update .py
    apiの部分

--- .py
+++ .py
@@ -7,18 +7,32 @@ from google import genai
 from google.genai import types
 from pydantic import BaseModel
 from typing import List
-
+#key
 load_dotenv()
 key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=key)
 app = FastAPI()
-
+#通信
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=[*],
-    allow_headers=[*]
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 @app.get("/")
 def read_root():
     return{"status":"ok"}
+class ChatRequest(BaseModel):
+    message:str
+#py=js
+@app.post("/api/chat")
+async def chat_endpoint(request: ChatRequest):
+    message = request.message
+    #API
+    response = client.models.generate_content(
+        model="gemini-3.1-flash-lite",
+        contents=message
+    )
+    ai_reply = response.text
+    return{"reply":ai_reply}
