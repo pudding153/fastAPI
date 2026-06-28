aca46723d723f52aa8ff9396ba60f0517a3ae088
pudding153 <subiriam777@gmail.com>
2026-06-28 19:15:58 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI,HTTPException,status
+from fastapi import FastAPI,HTTPException,status,Request
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
@@ -7,6 +7,7 @@ from google import genai
 from google.genai import types
 from pydantic import BaseModel
 from typing import List
+import types
 #key
 load_dotenv()
 key = os.getenv("GEMINI_API_KEY")
@@ -19,4 +20,17 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
-    
+)
+
+@app.get("/")
+def read_root():
+    return{"status":"ok"}
+
+#受取  #リクエスト
+@app.post("/api/chat")
+async def chat_endpoint(req: Request):
+    body = await req.json()
+    message = body.get("message","")
+#会話履歴
+    talk = body.get("history", [])
+    talk.append({"role":"user","parts":[{"text":message}]})
