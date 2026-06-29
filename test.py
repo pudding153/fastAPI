aa809bf6e1df4f9006d6719acc8340c6c7c40bf1
pudding153 <subiriam777@gmail.com>
2026-06-29 16:59:38 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 import os
+import logging
 from fastapi import FastAPI,HTTPException,status,Request
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
 from google import genai
 from google.genai import types
+from google.genai.errors import APIError
 from pydantic import BaseModel
 from typing import List
+
+logging.basicConfig(lebel=logging.INFO)
+logger = logging.getLOgger(__name__)
 #key
 load_dotenv()
 key = os.getenv("GEMINI_API_KEY")
@@ -28,26 +33,41 @@ def read_root():
 #受取  #リクエスト
 @app.post("/api/chat")
 async def chat_endpoint(req: Request):
-    body = await req.json()
-    message = body.get("message","")
+    try:
+        body = await req.json()
+        message = body.get("message","")
 #会話履歴
-    talk = body.get("history", [])
-    talk.append({"role":"user","parts":[{"text":message}]})
+        talk = body.get("history", [])
+        talk.append({"role":"user","parts":[{"text":message}]})
 #api設定
-    s = ("setting")
-    ai_config=types.GenerateContentConfig(
-        system_instruction=s,
-        max_output_tokens=200
+        s = ("setting")
+        ai_config=types.GenerateContentConfig(
+            system_instruction=s,
+            max_output_tokens=200
 )
-    response = client.models.generate_content(
-        contents=talk,
-        model="gemini-3.1-flash-lite",
-        config=ai_config
+        response = client.models.generate_content(
+            contents=talk,
+            model="gemini-3.1-flash-lite",
+            config=ai_config
 )
-    talk.append({"role":"model","parts":[{"text":response.text}]})
+        talk.append({"role":"model","parts":[{"text":response.text}]})
     #送り返す
-    return{
-        "success":True,
-        "reply":response.text,
-        "history":talk
+        return{
+            "success":True,
+            "reply":response.text,
+            "history":talk
     }
+    except APIError as e:
+        logger.error(f"Gemini API Error:{e}")
+        return{
+            "success":False,
+            "error":"AIerror",
+            "detail":str(e)
+        }
+    except Exception as e:
+        logger.error(f"Unexpected Error:{e}")
+        return{
+            "success":False,
+            "error":"error",
+            "detail":str(e)
+        }
